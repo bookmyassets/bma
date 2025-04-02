51fbf83c54b0a1a7c5bc7ee20bcefa9e27798862
bookmyassets <bookmyassets101@gmail.com>
2025-04-02 13:04:28 +0530

    whatsapp button

--- src/app/components/Callus.jsx
+++ src/app/components/Callus.jsx
@@ -5,7 +5,7 @@ import { Phone } from "lucide-react";
 export default function ContactNow() {
   return (
     <div>
-        <div className="fixed bottom-8 right-8 z-50">
+        <div className="fixed bottom-8 right-8 z-50 max-md:hidden">
           <button className="bg-yellow-500 hover:bg-yellow-600 text-black p-4 rounded-full shadow-lg flex items-center">
             <Phone className="mr-2" />{" "}
             <Link href="https://wa.me/919717671112" className="hidden md:inline">Contact Now</Link>
