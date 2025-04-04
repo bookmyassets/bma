2bfa2027d0659373c1b0e1101867da4739adca90
bookmyassets <bookmyassets101@gmail.com>
2025-04-04 19:29:38 +0530

    contact page

--- src/app/layout.js
+++ src/app/layout.js
@@ -287,12 +287,13 @@ export default function RootLayout({ children }) {
                     ref={getInTouchRef}
                     className="relative group"
                   >
+                    <Link href="/contact"> 
                     <button
                       className="text-[#FDB913] hover:text-white px-3 py-2 cursor-pointer flex items-center gap-1"
                       onClick={toggleGetInTouchDropdown}
                       onMouseEnter={() => setIsGetInTouchDropdownOpen(true)}
                       onMouseLeave={() => setIsGetInTouchDropdownOpen(false)}
-                    >
+                      >
                       Get in Touch
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -303,14 +304,15 @@ export default function RootLayout({ children }) {
                         viewBox="0 0 24 24"
                         stroke="currentColor"
                         strokeWidth="2"
-                      >
+                        >
                         <path
                           strokeLinecap="round"
                           strokeLinejoin="round"
                           d="M19 9l-7 7-7-7"
-                        />
+                          />
                       </svg>
                     </button>
+                          </Link>
 
                     <AnimatePresence>
                       {isGetInTouchDropdownOpen && (
