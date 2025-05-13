9d35b07dd2e0646f7765a2831302dfa7dfc87b0c
bookmyassets <bookmyassets101@gmail.com>
2025-05-13 16:26:03 +0530

    clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -189,6 +189,15 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+        <Script type="text/javascript">
+          {`
+               (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "rivub95ldd");
+        `}
+        </Script>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
