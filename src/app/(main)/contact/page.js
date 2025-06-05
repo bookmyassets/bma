6708135740b71d4d58b18cc7448dbca2cf44ab0b
bookmyassets <bookmyassets101@gmail.com>
2025-06-05 15:41:09 +0530

    changes

--- src/app/(main)/contact/page.js
+++ src/app/(main)/contact/page.js
@@ -145,7 +145,7 @@ const ContactPage = () => {
 
       // Send to TeleCRM
       const response = await fetch(
-        `https://api.telecrm.in/enterpriselead`,
+        `https://api.telecrm.in/enterprise/67a30ac2989f94384137c2ff/autoupdatelead`,
         {
           method: "POST",
           headers: {
