986f1cc4e3f5a201e85665be46b7f23efede7634
bookmyassets <bookmyassets101@gmail.com>
2025-06-26 18:33:52 +0530

    chnages

--- src/app/(main)/components/Projectinformation.jsx
+++ src/app/(main)/components/Projectinformation.jsx
@@ -18,40 +18,7 @@ export default function Projectinformation() {
         Request More Information
       </button>
 
-<AnimatePresence>
-        {isContactFormOpen && (
-          <motion.div
-            className=""
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            onClick={closeContactForm}
-          >
-            <motion.div
-              className=""
-              initial={{ scale: 0.95, opacity: 0 }}
-              animate={{ scale: 1, opacity: 1 }}
-              exit={{ scale: 0.95, opacity: 0 }}
-              onClick={(e) => e.stopPropagation()} 
-            >
-              <div className="">
-                <div className="">
-
-                  
-                </div>
-                <PopupForm
-                title="Get Expert Guidance on Dholera Investment"
-                buttonName="Book Consultation"
-                onClose={closeContactForm}
-              />
-                
-              </div>
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-
-{/*       <AnimatePresence>
+      <AnimatePresence>
         {isContactFormOpen && (
           <motion.div
             className="fixed inset-0 z-50 bg-black/50 flex justify-center items-center"
@@ -75,7 +42,7 @@ export default function Projectinformation() {
             </motion.div>
           </motion.div>
         )}
-      </AnimatePresence> */}
+      </AnimatePresence>
     </>
   )
 }
