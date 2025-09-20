ed1d9dd60ee32326aeb01279ade68c31b752d4d8
bookmyassets <bookmyassets101@gmail.com>
2025-09-20 18:12:19 +0530

    changes

--- src/app/(moreInfo)/more-info/layout.js
+++ src/app/(moreInfo)/more-info/layout.js
@@ -15,6 +15,11 @@ export default function RootLayout({ children }) {
           src="https://www.googletagmanager.com/gtag/js?id=G-NWBLCWMLDF"
         />
         <Script />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17011995425"
+        />
+        <Script />
 
         <Script
           id="google-analytics"
@@ -29,24 +34,24 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
-{/* GTM - BMA Landing Page */}
-        <Script type="text/javascript">
-          {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TXSQ62G6')
-
-                `}
-        </Script>
+{/* GTM - BMA Landing Page GTM only */}
         <Script type="text/javascript">
           {`
            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WLDR8CDN');
+           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+           })(window,document,'script','dataLayer','GTM-WLDR8CDN');
+           `}
+        </Script>
+        {/* GTM - AW tag */}
+        <Script type="text/javascript">
+          {`
+              window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17011995425');
                 `}
         </Script>
         <Script type="text/javascript">
