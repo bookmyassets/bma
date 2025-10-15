5d66a209fb94045974d38aa169ccf479bcf64415
bookmyassets <bookmyassets101@gmail.com>
2025-10-15 16:40:47 +0530

    changes

--- src/app/(main)/components/PopUpForm.jsx
+++ src/app/(main)/components/PopUpForm.jsx
@@ -29,7 +29,7 @@ export default function PopupForm() {
       const timer = setTimeout(() => {
         setShowFormPopup(true);
         sessionStorage.setItem('popupShownThisSession', 'true');
-      }, 5000); // 5 seconds
+      }, 8000); // 5 seconds
 
       return () => clearTimeout(timer);
     }
