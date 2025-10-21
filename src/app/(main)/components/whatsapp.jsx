c02957b27c481399eaa9556ef68a8b334e1e95c1
bookmyassets <bookmyassets101@gmail.com>
2025-10-21 13:50:44 +0530

    changes

--- src/app/(main)/components/whatsapp.jsx
+++ src/app/(main)/components/whatsapp.jsx
@@ -4,11 +4,13 @@ import { FaWhatsapp, FaPhoneAlt } from "react-icons/fa";
 const FloatingButtons = () => {
   return (
     <>
-      {/* Desktop View - Floating Buttons on Bottom Right */}
-      
-
       {/* Mobile View - Fixed Box at Bottom */}
-      <div className="fixed bottom-0 left-0 w-full bg-white shadow-md p-3 flex justify-around lg:hidden z-50">
+      <div
+        className="fixed bottom-0 inset-x-0 w-full bg-white shadow-md p-3 flex justify-around lg:hidden z-50"
+        style={{
+          paddingBottom: "calc(env(safe-area-inset-bottom) + 8px)", // ensures it's flush with screen bottom
+        }}
+      >
         {/* WhatsApp Button */}
         <a
           href="https://wa.me/918130371647"
@@ -18,6 +20,7 @@ const FloatingButtons = () => {
         >
           <FaWhatsapp size={24} className="mr-2" /> WhatsApp
         </a>
+
         {/* Call Button */}
         <a
           href="tel:+918130371647"
