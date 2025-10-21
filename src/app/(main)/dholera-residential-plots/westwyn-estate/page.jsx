0d55dc9f4c563de5aa00c2866d63fbd7e213fd54
bookmyassets <bookmyassets101@gmail.com>
2025-10-21 14:10:09 +0530

    changes

--- src/app/(main)/dholera-residential-plots/westwyn-estate/page.jsx
+++ src/app/(main)/dholera-residential-plots/westwyn-estate/page.jsx
@@ -299,6 +299,18 @@ export default function HeroCarousel() {
                     alt={image.alt}
                     className="w-full h-full object-cover"
                   />
+                  <button
+                  onClick={prevSlide}
+                  className="absolute left-4 top-1/2 -translate-y-1/2 z-10 bg-black/50 hover:bg-black/70 text-white p-2 rounded-full transition-colors"
+                >
+                  <ChevronLeft className="w-6 h-6" />
+                </button>
+                <button
+                  onClick={nextSlide}
+                  className="absolute right-4 top-1/2 -translate-y-1/2 z-10 bg-black/50 hover:bg-black/70 text-white p-2 rounded-full transition-colors"
+                >
+                  <ChevronRight className="w-6 h-6" />
+                </button>
                 </div>
               ))}
 
