bf494bd79027cc113eb60d045fbf8df2db2f93e9
bookmyassets <bookmyassets101@gmail.com>
2026-01-07 12:42:14 +0530

    GTM added

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -50,13 +50,16 @@ fbq('track', 'PageView');`}
           ;
         </Script>
 
-        <Script strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      
+    
+        <Script type="text/javascript">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MJ55GJFP')`}
-          ;
+})(window,document,'script','dataLayer','GTM-5CXXQ9DJ');
+           `}
         </Script>
 
         <Script
@@ -92,7 +95,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         `}</style>
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJ55GJFP"
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CXXQ9DJ"
     height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
