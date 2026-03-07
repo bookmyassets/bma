26d4f5035b1bb4edfac55f5f91845d841c58a225
bookmyassets <bookmyassets101@gmail.com>
2026-03-07 16:13:10 +0530

    changes

--- src/app/(main)/bulk-land/residential/page.jsx
+++ src/app/(main)/bulk-land/residential/page.jsx
@@ -74,11 +74,11 @@ export default function page() {
               </div>
               <div className="flex flex-col items-center">
                 <h3 className="text-gray-800 font-semibold text-xs md:text-sm mb-1">
-                  Future Growth
+                  Get Brochure
                 </h3>
-                <p className="text-gray-700 text-sm md:text-lg font-medium">
-                  High ROI
-                </p>
+                <a href="https://cdn.sanity.io/files/c3e1h345/projects/66dcad140036ee16f4c1d0d40cb3f56d39841639.pdf" target="_blank" className="text-blue-400 underline text-sm md:text-lg font-medium">
+                  Brochure
+                </a>
               </div>
             </div>
           </div>
