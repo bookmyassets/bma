bd838b1569f669d324ea1a510acae0d345ca25c9
--global <namangupta2360@gmail.com>
2026-03-23 18:16:43 +0530

    changes

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -61,6 +61,22 @@ export default function RootLayout({ children }) {
   j.async=true;
   j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
   f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GT-T5PQ3QDN');`,
+          }}
+        />
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){
+  w[l]=w[l]||[];
+  w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+  var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),
+      dl=l!='dataLayer'?'&l='+l:'';
+  j.async=true;
+  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+  f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTM_ID}');`,
           }}
         />
@@ -159,6 +175,14 @@ export default function RootLayout({ children }) {
           ── GTM noscript body tag ─────────────────────────────────────────
           Must be the first child of <body> per GTM spec.
         */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GT-T5PQ3QDN`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <noscript>
           <iframe
             src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
