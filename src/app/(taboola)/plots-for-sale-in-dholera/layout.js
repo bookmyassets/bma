adf515c720eedd442392fd2a083f7827affee896
--global <namangupta2360@gmail.com>
2026-04-14 15:25:55 +0530

    changes

--- src/app/(taboola)/plots-for-sale-in-dholera/layout.js
+++ src/app/(taboola)/plots-for-sale-in-dholera/layout.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import './globals.css';
+import "./globals.css";
 import Form from "./components/Form";
 import FloatingButtons from "./components/Whatsapp";
 import Script from "next/script";
 
 export default function layout({ children }) {
-
   return (
     <html>
       <head>
@@ -20,13 +19,24 @@ export default function layout({ children }) {
           content="premium plots in dholera, residential plots in dholera, dholera smart city plots, bookmyassets plots, dholera investment plots, govt approved dholera plots"
         />
 
+        <Script
+          id="ms-clarity"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wbi9ww24ro");`,
+          }}
+        />
       </head>
       <body>
         <div>
           <div>
             <Navbar />
             {children}
-            <FloatingButtons/>
+            <FloatingButtons />
           </div>
         </div>
       </body>
