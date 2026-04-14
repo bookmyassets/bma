001a2bcb9d794f5a91c200ed5d0198a01f62909b
--global <namangupta2360@gmail.com>
2026-04-14 17:02:19 +0530

    GTM Twitter

--- src/app/(moreInfo)/dholera-smart-city/layout.js
+++ src/app/(moreInfo)/dholera-smart-city/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-ND5WPPD5');
+})(window,document,'script','dataLayer','GTM-PX4R9NV5');
            `}
         </Script>
         <Script>
@@ -54,7 +54,7 @@ twq('config','oxi2l');`}
 
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-ND5WPPD5"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PX4R9NV5"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
