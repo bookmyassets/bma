7f4f7e0c749ad488eef4dc28ba60e5264688492f
bookmyassets <bookmyassets101@gmail.com>
2026-04-28 15:28:56 +0530

    Update layout.js

--- src/app/(taboola)/plots-for-sale-in-dholera/layout.js
+++ src/app/(taboola)/plots-for-sale-in-dholera/layout.js
@@ -27,7 +27,7 @@ export default function layout({ children }) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WRB4QS44')`,
+})(window,document,'script','dataLayer','GTM-58263M3B')`,
           }}
         />
 
@@ -68,7 +68,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <body>
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GTM-WRB4QS44`}
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-58263M3B`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
