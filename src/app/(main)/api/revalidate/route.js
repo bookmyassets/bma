770c396b1e122bfa1b8708d7229c6f191ffac2bb
tdd-dholera <topdealsdholera@gmail.com>
2026-05-18 13:10:02 +0530

    changes

--- src/app/(main)/api/revalidate/route.js
+++ src/app/(main)/api/revalidate/route.js
@@ -1,13 +1,17 @@
 // app/api/revalidate/route.js
-import { revalidateTag } from 'next/cache'
-import { NextResponse } from 'next/server'
+import { revalidatePath, revalidateTag } from "next/cache";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const secret = request.nextUrl.searchParams.get('secret')
+  const secret = request.nextUrl.searchParams.get("secret");
   if (secret !== process.env.REVALIDATION_SECRET) {
-    return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
+    return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   }
 
-  revalidateTag('redirect')
-  return NextResponse.json({ revalidated: true })
+  if (type === "post") {
+    revalidatePath("/sitemap.xml");
+  }
+
+  revalidateTag("redirect");
+  return NextResponse.json({ revalidated: true });
 }
