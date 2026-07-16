ee03e326a71e2689c964177ff2cb38fff67a1e54
Naman <namangupta2360@gmail.com>
2026-07-16 18:36:55 +0530

    changes

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -192,7 +192,7 @@ export default function page() {
         <Hero />
         <Dholera />
         <WestWyn />
-        <BMA />
+        {/* <BMA /> */}
         <HowToBuy />
         <LatestUpdates />
         <FAQSection />
