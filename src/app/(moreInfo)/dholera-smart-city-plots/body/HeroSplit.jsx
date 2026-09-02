eabb8f955a513290b8f9d43740dcd2a38a7f6ef1
Naman <namangupta2360@gmail.com>
2026-09-02 15:23:40 +0530

    changes

--- src/app/(moreInfo)/dholera-smart-city-plots/body/HeroSplit.jsx
+++ src/app/(moreInfo)/dholera-smart-city-plots/body/HeroSplit.jsx
@@ -4,8 +4,8 @@ import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import { useRouter, usePathname } from "next/navigation";
 import logo from "@/assests/ad-page/dholera-govt-logo.webp";
-import img1 from "@/assests/google-landing-page-banner-desktop.webp";
-import mobileImg from "@/assests/google-landing-page-banner-mobile.webp";
+import img1 from "@/assests/ad-page/dholera-investment-desktop-banner-landing-page.webp";
+import mobileImg from "@/assests/ad-page/dholera-investment-mobile-banner-landing-page.webp";
 /* import mobileImg from "@/assests/rakhi-offer-bookmyassets-landing-page-mobile.webp"; */
 import BrochureDownload from "../components/BrochureDownload";
 
