a86ba29c412c33ab606c124e3d8b2190d42aef06
tdd-dholera <bookmyassets101@gmail.com>
2026-09-05 16:53:07 +0530

    new-navbar

--- src/app/(moreInfo)/dholera-smart-city-plots/layout.js
+++ src/app/(moreInfo)/dholera-smart-city-plots/layout.js
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Navbar from "@/app/(main)/components/Navbar";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 import Footer from "./body/Footer";
 import FloatingButtons from "./components/whatsapp";
