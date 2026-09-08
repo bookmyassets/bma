b2c4431c4bfd1da6d2dcce4e9d83c35bedd3909d
tdd-dholera <bookmyassets101@gmail.com>
2026-09-08 13:23:54 +0530

    fix missing WestWyn contact form module

---
+++ src/app/(moreInfo)/dholera-smart-city-plots/components/WestwynContactForm.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { FaChartLine, FaMapMarkedAlt, FaPhoneAlt } from "react-icons/fa";
+import GetinTouch from "./GetinTouch";
+
+const westwynFormCtas = [
+  { label: "Get Investment Details", icon: FaChartLine },
+  { label: "Get Plot Details", icon: FaMapMarkedAlt },
+  { label: "Get A Call Back", icon: FaPhoneAlt },
+];
+
+export default function WestwynContactForm({ onClose }) {
+  return (
+    <GetinTouch
+      title="Register Ready Plots In Dholera From 8 Lakh"
+      buttonName="Get A Call Back"
+      ctaItems={westwynFormCtas}
+      onClose={onClose}
+    />
+  );
+}
